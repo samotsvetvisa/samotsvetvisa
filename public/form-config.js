754b66cceea0eae831e0564e0e5359edf2d75ffd
samotsvetvisa <karfagen38@gmail.com>
2026-08-13 22:52:15 +0300

    Connect enquiry form to Google Sheets

--- public/form-config.js
+++ public/form-config.js
@@ -1,1 +1,1 @@
-window.SAMOTSVET_FORM_ENDPOINT = "";
+window.SAMOTSVET_FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycbyNYUU3_kr6U4uDzjTfUhswXSyO3xMzXFE9Uvb-QciDeNOmaD82-0d4dWQbBPhXKl-xng/exec";
